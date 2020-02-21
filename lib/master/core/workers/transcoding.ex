605dcd7f45ba5c3b28d441b7fcd1d23830607724
denislutski <avenger_den@mail.ru>
2020-02-21 18:36:01 +0300

    Fix origin_url
    https://fun.myjetbrains.com/youtrack/issue/FUN-0

--- lib/master/core/workers/transcoding.ex
+++ lib/master/core/workers/transcoding.ex
@@ -4,7 +4,7 @@ defmodule Workers.Transcoding do
 
   def call(worker, task) do
     name = task.origin_url |> String.split("/") |> List.last()
-    string_args = "#{task.movie_uuid} #{task.origin_server_ip} #{task.origin_url} #{name} #{task.server.name}"
+    string_args = "#{task.movie_uuid} #{task.origin_server_ip} /mnt/#{task.origin_url} #{name} #{task.server.name}"
     args = ["cd /home/scripts; nohup python3 transcode.py #{string_args} > nohup.out /dev/null 2>&1 &"]
 
     case System.cmd("ssh", Workers.BuildSshArgs.call(worker, args), stderr_to_stdout: true) do
