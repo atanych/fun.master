abeffdd8e6e6ab8fc8411ab45755fd1243e4ed1c
denislutski <avenger_den@mail.ru>
2020-04-01 14:06:52 +0300

    Fix tar logic

--- lib/master/core/tasks/tar.ex
+++ lib/master/core/tasks/tar.ex
@@ -10,22 +10,12 @@ defmodule Tasks.Tar do
 
     Ext.System.cmd!("tar", ["czf", "#{folder_path}.tar", folder_path])
 
-#    Ext.System.cmd!("rm", ["-R", folder_path])
+    Ext.System.cmd!("rm", ["-R", folder_path])
 
     Master.Repo.save!(task, status: :done)
   rescue
     e in RuntimeError ->
       Logger.warn("Task to tar has error - #{inspect(e)}")
-
-      # Hack, tar exists but we have error
-      folder_path_list = task.url |> String.split("/") |> Enum.drop(-1)
-
-      if File.exists?(Enum.join(folder_path_list, "/") <> ".tar") do
-        Logger.warn("Task exists - #{inspect(task)}")
-        Master.Repo.save!(task, status: :done)
-      else
-        Logger.warn("Task not exists - #{inspect(task)}")
-        Master.Repo.save!(task, status: :new)
-      end
+      Master.Repo.save!(task, status: :new, url: nil)
   end
 end
